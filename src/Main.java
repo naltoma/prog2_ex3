2e6d9e16d6ea60b1a45af2f2016a7209ebc9b90b
Naruaki TOMA <tnal@ie.u-ryukyu.ac.jp>
2017-11-21 16:27:18 +0900

    add ExDice.java for ex3_3

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,10 @@ import jp.ac.uryukyu.ie.ex3.*;
 
 public class Main {
     public static void main(String[] args){
-	Dice dice = new Dice();
-	dice.play();
-	System.out.println(dice.getValue());
+	ExDice exdice = new ExDice(5, 7);
+	for(int i=0; i<10; i++){
+	    exdice.play();
+	    System.out.println(exdice.getValue());
+	}
     }
 }
